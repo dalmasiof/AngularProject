734b71e7532f15d2bd2a6b805dfa2c16253a85ae
Dalmasio <dalmasiof@gmail.com>
2021-08-13 22:12:52 -0300

    startfolder to separe contents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CommunicationComponentComponent } from './communication-component/communication-component.component';
-import { InputBindingComponent } from './input-binding/input-binding.component';
-import { ClientComponentComponent } from './client-component/client-component.component';
-import { EventComponent } from './event/event.component';
+import { CommunicationComponentComponent } from './ComunicationCOmps/communication-component/communication-component.component';
+import { InputBindingComponent } from './ComunicationCOmps/input-binding/input-binding.component';
+import { ClientComponentComponent } from './ComunicationCOmps/client-component/client-component.component';
+import { EventComponent } from './ComunicationCOmps/event/event.component';
 import{ MatCardModule} from '@angular/material/card';
 import{ MatButtonModule} from '@angular/material/button';
 import{ MatIconModule} from '@angular/material/icon';
@@ -15,20 +15,20 @@ import{ MatFormFieldModule} from '@angular/material/form-field';
 import{ MatInputModule} from '@angular/material/input';
 
 
-import { ChildComponent } from './event/child/child.component';
-import { ClientsComponent } from './clients/clients.component';
+import { ChildComponent } from './ComunicationCOmps/event/child/child.component';
+import { ClientsComponent } from './ComunicationCOmps/clients/clients.component';
 import { FormsModule } from '@angular/forms';
-import { ItemCLientComponent } from './clients/item-client/item-client.component';
+import { ItemCLientComponent } from './ComunicationCOmps/clients/item-client/item-client.component';
 
-import { NameChangesComponent } from './on-change/name-changes/name-changes.component';
-import { OnChangeComponent } from './on-change/on-change/on-change.component';
-import { InterceptingComponent } from './intercepting/intercepting.component';
-import { NameComponent } from './intercepting/name/name.component';
-import { MainLifeCycleComponent } from './LifeCycle/main-life-cycle/main-life-cycle.component';
+import { NameChangesComponent } from './LifeCycleComps/on-change/name-changes/name-changes.component';
+import { OnChangeComponent } from './LifeCycleComps/on-change/on-change/on-change.component';
+import { InterceptingComponent } from './ComunicationCOmps/intercepting/intercepting.component';
+import { NameComponent } from './ComunicationCOmps/intercepting/name/name.component';
+import { MainLifeCycleComponent } from './LifeCycleComps/on-change/LifeCycle/main-life-cycle/main-life-cycle.component';
 import { MatLineModule, MatOptionModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatListModule } from '@angular/material/list';
-import { MmainLifeCicleChildComponent } from './LifeCycle/mmain-life-cicle-child/mmain-life-cicle-child.component';
+import { MmainLifeCicleChildComponent } from './LifeCycleComps/on-change/LifeCycle/mmain-life-cicle-child/mmain-life-cicle-child.component';
 
 
 @NgModule({
